6126e5f28d7e80057625785b45a6475e8a681264
salemebo <salem.ebo@gmail.com>
2021-06-27 07:12:34 +0200

    full & any

--- src/35/351.ts
+++ src/35/351.ts
@@ -26,124 +26,233 @@ export function solve351() {
 	for (let i = 0; i < testCount; i++) {
 		const test = input[lastLine++].split(',').map(s => s.split('-').map(Number)) as [number, number][];
 
-		let index = -1;
+		const match = searchIndex(delta, test, paths);
+		console.log(match, i, testCount - i);
 
-		whileLoop:
-		do {
-			const match = findFirstMatch(++index, delta, test, paths);
-			if (match.index === paths.length) {
-				appendOutput(`-1 0\n`);
-				continue testLoop;
-			}
-			const isoForm = match.isoForm!;
-			// start match
-			let matchCount = isFullMatching(delta, test[0], isoForm[match.start!]) ? 1 : 0;
-			// end match
-			const se = isStartMatching(delta, test[test.length - 1], isoForm[match.start! + test.length - 1]);
-			if (!se) {
-				// not match, break chain
-				index = match.index;
-				continue whileLoop;
-			}
-			const ee = isEndMatching(delta, test[test.length - 1], isoForm[match.start! + test.length - 1]);
-			if (ee) {
-				matchCount++;
-			}
+		appendOutput(`${match.index} ${match.count}\n`);
 
-			for (let j = 1, s = match.start! + 1; j < test.length - 1 && s < isoForm.length - 1; j++, s++) {
-				const isFull = isFullMatching(delta, test[j], isoForm[s]);
-				if (isFull) {
-					matchCount++;
-				} else {
-					// not match, break chain
-					index = match.index;
-					continue whileLoop;
-				}
-			}
-			if (matchCount == 0) {
-				index = match.index;
-				continue whileLoop;
-			}
-			appendOutput(`${match.index} ${matchCount}\n`);
-			continue testLoop;
-		} while (index <= paths.length);
-	}
-}
+		// let index = -1;
 
-export function findFullMatch(index: number, delta: number, test: [number, number], isoForms: [number, number][][]) {
-	for (; index < isoForms.length; index++) {
-		const isoForm = isoForms[index];
-		if (isFullMatching(delta, test, isoForm[0])) {
-			return { index, start: 0, isoForm };
-		}
-	}
-	return { index };
-}
-
-export function findFullMatchIn(index: number, delta: number, test: [number, number], isoForms: [number, number][][]) {
-	for (; index < isoForms.length; index++) {
-		const isoForm = isoForms[index];
-		const start = sortedIndexBy(isoForm, test, a => isFullMatching(delta, test, a));
-		if (start < isoForm.length && start > 0) {
-			return { index, start, isoForm };
-		}
-	}
-	return { index };
-}
-
-
-export function findFirstMatch(index: number, delta: number, tests: [number, number][], isoForms: [number, number][][]) {
-	for (; index < isoForms.length; index++) {
-		const isoForm = isoForms[index];
-		if (isoForm.length < tests.length) {
-			continue;
-		}
-		if (isEndMatching(delta, tests[0], isoForm[0]) && isStartMatching(delta, tests[1], isoForm[1])) {
-			return { index, start: 0, isoForm };
-		}
-		// if (tests[0][0] > isoForm[0][1]) {
-		// 	const start = sortedIndexBy(isoForm, tests[0], a => isStartMatching(delta, tests[0], a));
-		// 	if (start < isoForm.length && start > 0) {
-		// 		return { index, start, isoForm };
+		// whileLoop:
+		// do {
+		// 	const match = findFirstMatch(++index, delta, test, paths);
+		// 	if (match.index === paths.length) {
+		// 		appendOutput(`-1 0\n`);
+		// 		continue testLoop;
 		// 	}
-		// }
+		// 	const isoForm = match.isoForm!;
+		// 	// start match
+		// 	let matchCount = isFullMatching(delta, test[0], isoForm[match.start!]) ? 1 : 0;
+		// 	// end match
+		// 	const se = isStartMatching(delta, test[test.length - 1], isoForm[match.start! + test.length - 1]);
+		// 	if (!se) {
+		// 		// not match, break chain
+		// 		index = match.index;
+		// 		continue whileLoop;
+		// 	}
+		// 	const ee = isEndMatching(delta, test[test.length - 1], isoForm[match.start! + test.length - 1]);
+		// 	if (ee) {
+		// 		matchCount++;
+		// 	}
+
+		// 	for (let j = 1, s = match.start! + 1; j < test.length - 1 && s < isoForm.length - 1; j++, s++) {
+		// 		const isFull = isFullMatching(delta, test[j], isoForm[s]);
+		// 		if (isFull) {
+		// 			matchCount++;
+		// 		} else {
+		// 			// not match, break chain
+		// 			index = match.index;
+		// 			continue whileLoop;
+		// 		}
+		// 	}
+		// 	if (matchCount == 0) {
+		// 		index = match.index;
+		// 		continue whileLoop;
+		// 	}
+		// 	appendOutput(`${match.index} ${matchCount}\n`);
+		// 	continue testLoop;
+		// } while (index <= paths.length);
 	}
-	return { index };
 }
 
-export function isConnectMatch(delta: number, tests: [number, number][], isoForms: [number, number][], testStart: number, isoStart: number) {
-	for (let i = testStart; i < tests.length; i++) {
-		if (!(isEndMatching(delta, tests[i], isoForms[isoStart + i]) && isStartMatching(delta, tests[i + 1], isoForms[isoStart + i]))) {
-			return false;
+export function searchIndex(delta: number, test: [number, number][], isoForms: [number, number][][]) {
+	fullSearch:
+	for (let index = 0; index < isoForms.length; index++) {
+		const isoForm = isoForms[index];
+		for (let x = 0; x < isoForm.length; x++) {
+			if (isInBlock(delta, test[0], isoForm[x])) {
+				const count = getMatchCount(delta, test, isoForm, x);
+				if (count > 0) {
+					return { index, count };
+				}
+				continue fullSearch;
+			}
 		}
 	}
-	return true;
+	anySearch:
+	for (let index = 0; index < isoForms.length; index++) {
+		const isoForm = isoForms[index];
+		for (let x = 0; x < isoForm.length; x++) {
+			if (isEndInBlock(delta, test[0], isoForm[x])) {
+				const count = getMatchCount(delta, test, isoForm, x);
+				if (count > 0) {
+					return { index, count };
+				}
+				continue anySearch;
+			}
+		}
+	}
+	return { index: -1, count: 0 };;
 }
 
-
-export function isStartMatching(delta: number, test: [number, number], isoForm: [number, number]) {
-	return isMatching(0, delta, test, isoForm);
+export function getMatchCount(delta: number, test: [number, number][], isoForms: [number, number][], start: number) {
+	if (test.length > isoForms.length - start) {
+		return 0;
+	}
+	let matchCount = 0;
+	for (let i = 0, x = start; i < test.length; i++, x++) {
+		const testBlock = test[i];
+		const isoBlock = isoForms[x];
+		if (i == 0) {
+			if (isInBlock(delta, testBlock, isoBlock)) {
+				matchCount++;
+				continue;
+			}
+			if (isEndInBlock(delta, testBlock, isoBlock)) {
+				continue;
+			}
+			return 0;
+		} else if (i < test.length - 1) {
+			if (isInBlock(delta, testBlock, isoBlock)) {
+				matchCount++;
+				continue;
+			}
+			return 0;
+		} else if (i == test.length - 1) {
+			if (isInBlock(delta, testBlock, isoBlock)) {
+				matchCount++;
+				break;
+			}
+			if (isStartInBlock(delta, testBlock, isoBlock)) {
+				break;
+			}
+			return 0;
+		}
+	}
+	return matchCount;
 }
 
-export function isEndMatching(delta: number, test: [number, number], isoForm: [number, number]) {
-	return isMatching(1, delta, test, isoForm);
-}
+export function isInBlock(delta: number, test: [number, number], isoForm: [number, number]) {
 
-export function isFullMatching(delta: number, test: [number, number], isoForm: [number, number]) {
-	return isMatching(0, delta, test, isoForm) && isMatching(1, delta, test, isoForm);
-}
-
-export function isNotFullMatching(delta: number, test: [number, number], isoForm: [number, number]) {
-	return !isMatching(0, delta, test, isoForm) || !isMatching(1, delta, test, isoForm);
-}
-
-export function isMatching(index: number, delta: number, test: [number, number], isoForm: [number, number]) {
-	if (test[index] == isoForm[index]) {
+	if (test[0] == isoForm[0] && test[1] == isoForm[1]) {
 		return true;
 	}
-	// can correct the error
-	else if (Math.abs(isoForm[index] - test[index]) <= delta) {
+
+	if (Math.abs(isoForm[0] - test[0]) <= delta && Math.abs(isoForm[1] - test[1]) <= delta) {
 		return true;
 	}
 	return false;
 }
+
+export function isStartInBlock(delta: number, test: [number, number], isoForm: [number, number]) {
+
+	if (test[0] == isoForm[0]) {
+		return true;
+	}
+
+	if (Math.abs(isoForm[0] - test[0]) <= delta) {
+		return true;
+	}
+	return false;
+}
+
+export function isEndInBlock(delta: number, test: [number, number], isoForm: [number, number]) {
+
+	if (test[1] == isoForm[1]) {
+		return true;
+	}
+	if (Math.abs(isoForm[1] - test[1]) <= delta) {
+		return true;
+	}
+	return false;
+}
+
+
+
+// export function findFullMatch(index: number, delta: number, test: [number, number], isoForms: [number, number][][]) {
+// 	for (; index < isoForms.length; index++) {
+// 		const isoForm = isoForms[index];
+// 		if (isFullMatching(delta, test, isoForm[0])) {
+// 			return { index, start: 0, isoForm };
+// 		}
+// 	}
+// 	return { index };
+// }
+
+// export function findFullMatchIn(index: number, delta: number, test: [number, number], isoForms: [number, number][][]) {
+// 	for (; index < isoForms.length; index++) {
+// 		const isoForm = isoForms[index];
+// 		const start = sortedIndexBy(isoForm, test, a => isFullMatching(delta, test, a));
+// 		if (start < isoForm.length && start > 0) {
+// 			return { index, start, isoForm };
+// 		}
+// 	}
+// 	return { index };
+// }
+
+
+// export function findFirstMatch(index: number, delta: number, tests: [number, number][], isoForms: [number, number][][]) {
+// 	for (; index < isoForms.length; index++) {
+// 		const isoForm = isoForms[index];
+// 		if (isoForm.length < tests.length) {
+// 			continue;
+// 		}
+// 		if (isEndMatching(delta, tests[0], isoForm[0]) && isStartMatching(delta, tests[1], isoForm[1])) {
+// 			return { index, start: 0, isoForm };
+// 		}
+// 		// if (tests[0][0] > isoForm[0][1]) {
+// 		// 	const start = sortedIndexBy(isoForm, tests[0], a => isStartMatching(delta, tests[0], a));
+// 		// 	if (start < isoForm.length && start > 0) {
+// 		// 		return { index, start, isoForm };
+// 		// 	}
+// 		// }
+// 	}
+// 	return { index };
+// }
+
+// export function isConnectMatch(delta: number, tests: [number, number][], isoForms: [number, number][], testStart: number, isoStart: number) {
+// 	for (let i = testStart; i < tests.length; i++) {
+// 		if (!(isEndMatching(delta, tests[i], isoForms[isoStart + i]) && isStartMatching(delta, tests[i + 1], isoForms[isoStart + i]))) {
+// 			return false;
+// 		}
+// 	}
+// 	return true;
+// }
+
+
+// export function isStartMatching(delta: number, test: [number, number], isoForm: [number, number]) {
+// 	return isMatching(0, delta, test, isoForm);
+// }
+
+// export function isEndMatching(delta: number, test: [number, number], isoForm: [number, number]) {
+// 	return isMatching(1, delta, test, isoForm);
+// }
+
+// export function isFullMatching(delta: number, test: [number, number], isoForm: [number, number]) {
+// 	return isMatching(0, delta, test, isoForm) && isMatching(1, delta, test, isoForm);
+// }
+
+// export function isNotFullMatching(delta: number, test: [number, number], isoForm: [number, number]) {
+// 	return !isMatching(0, delta, test, isoForm) || !isMatching(1, delta, test, isoForm);
+// }
+
+// export function isMatching(index: number, delta: number, test: [number, number], isoForm: [number, number]) {
+// 	if (test[index] == isoForm[index]) {
+// 		return true;
+// 	}
+// 	// can correct the error
+// 	else if (Math.abs(isoForm[index] - test[index]) <= delta) {
+// 		return true;
+// 	}
+// 	return false;
+// }
