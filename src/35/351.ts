ed689af984d1cf2dcbd33d3f26002c9d4eed6e57
salemebo <salem.ebo@gmail.com>
2021-07-01 13:09:01 +0200

    solve fisrt

--- src/35/351.ts
+++ src/35/351.ts
@@ -1,9 +1,16 @@
 import { appendOutput, readInfo, readInput, writeOutput } from '../read.js';
 
 import lodash from 'lodash';
-const { max, find, uniq, sortedIndexBy } = lodash;
+import { type } from 'os';
+const { min, max, find, uniq, sortedIndexBy } = lodash;
 
 export type Contact = { a: number, b: number, p: number };
+
+export type Cell = { start: number; end: number; };
+
+export type Coordinate = [Cell, Cell];
+export type IsoForm = Coordinate[];
+
 
 export function solve351() {
 
@@ -13,27 +20,33 @@ export function solve351() {
 	input.splice(0, 1);
 
 	let lastLine = 0;
-	const paths: [number, number][][] = new Array(n);
+	const isoForms: IsoForm[] = new Array(n);
 	for (let i = 0; i < n; i++) {
-		paths[i] = input[lastLine++].split(',').map(s => s.split('-').map(Number)) as [number, number][];
+		isoForms[i] = input[lastLine++]
+			.split(',')
+			.map(s => s
+				.split('-')
+				.map(Number)
+				.map(m => ({ start: m - delta, end: m + delta })) as Coordinate
+			);
 	}
 
 	const testCount = Number(input[lastLine++]);
 
-	// console.log(testCount, n, delta, paths);
 	writeOutput('');
 	testLoop:
 	for (let i = 0; i < testCount; i++) {
 		const test = input[lastLine++].split(',').map(s => s.split('-').map(Number)) as [number, number][];
 
-		const match = searchIndex(delta, test, paths);
+		const match = findBestMath(delta, test, isoForms);
 		console.log(match.index, match.count);
 
 		appendOutput(`${match.index} ${match.count}\n`);
 	}
 }
 
-export function searchIndex(delta: number, test: [number, number][], isoForms: [number, number][][]) {
+export function findBestMath(delta: number, test: [number, number][], isoForms: IsoForm[]) {
+	const matches: number[] = [];
 	fullSearch:
 	for (let index = 0; index < isoForms.length; index++) {
 		const isoForm = isoForms[index];
@@ -44,68 +57,38 @@ export function searchIndex(delta: number, test: [number, number][], isoForms: [
 			if (isoForm.length - x < test.length) {
 				continue fullSearch;
 			}
-			if (isEndInBlock(delta, test[0], isoForm[x])) {
-				const count = getMatchCount(delta, test, isoForm, x);
-				if (count > 0) {
-					return { index, count };
+			if (isInBlockNoDelta(test[0], isoForm[x])) {
+				if (getMatchCount(test, isoForm, x)) {
+					matches.push(index);
 				}
 				continue fullSearch;
 			}
 		}
 	}
-	return { index: -1, count: 0 };;
+	if (matches.length === 0) {
+		return { index: -1, count: 0 };
+	}
+	return { index: min(matches), count: matches.length };
 }
 
-export function getMatchCount(delta: number, test: [number, number][], isoForms: [number, number][], start: number) {
-
-	let matchCount = isInBlock(delta, test[0], isoForms[start]) ? 1 : 0;
-	if (isInBlock(delta, test[test.length - 1], isoForms[start + test.length - 1])) {
-		matchCount++;
-	} else if (!isStartInBlock(delta, test[test.length - 1], isoForms[start + test.length - 1])) {
-		return 0;
-	}
-	for (let i = 1, x = start + 1, l = test.length - 1; i < l; i++, x++) {
-		if (isInBlock(delta, test[i], isoForms[x])) {
-			matchCount++;
-			continue;
+export function getMatchCount(test: [number, number][], isoForm: IsoForm, start: number) {
+	for (let i = 0, x = start, l = test.length; i < l; i++, x++) {
+		if (!isInBlockNoDelta(test[i], isoForm[x])) {
+			return false;
 		}
-		return 0;
 	}
-	return matchCount;
+	return true;
 }
 
-export function isInBlock(delta: number, test: [number, number], isoForm: [number, number]) {
-
-	if (test[0] == isoForm[0] && test[1] == isoForm[1]) {
-		return true;
-	}
-
-	if (Math.abs(isoForm[0] - test[0]) <= delta && Math.abs(isoForm[1] - test[1]) <= delta) {
-		return true;
-	}
-	return false;
+export function isInBlockNoDelta(test: [number, number], isoForm: Coordinate) {
+	return inRangeOfCellStart(test, isoForm) && inRangeOfCellEnd(test, isoForm)
 }
 
-export function isStartInBlock(delta: number, test: [number, number], isoForm: [number, number]) {
-
-	if (test[0] == isoForm[0]) {
-		return true;
-	}
-
-	if (Math.abs(isoForm[0] - test[0]) <= delta) {
-		return true;
-	}
-	return false;
+export function inRangeOfCellStart(test: [number, number], isoForm: Coordinate) {
+	return test[0] >= isoForm[0].start && test[0] <= isoForm[0].end
 }
 
-export function isEndInBlock(delta: number, test: [number, number], isoForm: [number, number]) {
-
-	if (test[1] == isoForm[1]) {
-		return true;
-	}
-	if (Math.abs(isoForm[1] - test[1]) <= delta) {
-		return true;
-	}
-	return false;
+export function inRangeOfCellEnd(test: [number, number], isoForm: Coordinate) {
+	return test[1] >= isoForm[1].start && test[1] <= isoForm[1].end
 }
 
